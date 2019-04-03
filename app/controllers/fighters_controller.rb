e83b460e9e86be137206b2f438b015dba5eff405
Callixte Cauchois <callixte@gmail.com>
2019-04-03 15:49:52 +0200

    Etape 2: take 2
    CRUD completed

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -8,13 +8,38 @@ class FightersController < ApplicationController
   end
 
   def new
+    @fighter = Fighter.new
+  end
+
+  def edit
+    @fighter = Fighter.find(params[:id])
   end
 
   def create
     @fighter = Fighter.new(fighter_params)
 
-    @fighter.save
-    redirect_to @fighter
+    if @fighter.save
+      redirect_to @fighter
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @fighter = Fighter.find(params[:id])
+
+    if @fighter.update(fighter_params)
+      redirect_to @fighter
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @fighter = Fighter.find(params[:id])
+    @fighter.destroy
+
+    redirect_to fighters_path
   end
 
   private
