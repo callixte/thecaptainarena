d47546384b0c1426dff95fdd3b7eb9e1fe0e39b9
Callixte Cauchois <callixte@gmail.com>
2019-04-04 12:09:29 +0200

    Refacto pour preparer les tests

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -7,29 +7,6 @@ class FightsController < ApplicationController
   def go
     @fighter1 = Fighter.find(params[:id1])
     @fighter2 = Fighter.find(params[:id2])
-    life1 = @fighter1.life_points
-    life2 = @fighter2.life_points
-    the_tale = ''
-
-    while (life1 > 0) && (life2 > 0)
-      damage1, damage2, summary = round(@fighter1, @fighter2)
-      life1 -= damage1
-      life2 -= damage2
-      the_tale += summary
-    end
-
-    @fight = Fight.new
-    if life1 <= 0
-      @fight.loser = @fighter1
-      @fight.winner = @fighter2
-    else
-      @fight.loser = @fighter2
-      @fight.winner = @fighter1
-    end
-    @fight.summary = the_tale
-  end
-
-  def round(fighter1, fighter2)
-    return 23, 27, "#{fighter1.name} donne un coup de tête et #{fighter2.name} lui renvoie un coup de pied\n"
+    @fight = Fight.start(@fighter1, @fighter2)
   end
 end
--- test/controllers/fights_controller_test.rb
+++ test/controllers/fights_controller_test.rb
@@ -2,13 +2,17 @@ require 'test_helper'
 
 class FightsControllerTest < ActionDispatch::IntegrationTest
   test "should get select" do
-    get fights_select_url
+    get select_fight_url
     assert_response :success
   end
 
   test "should get go" do
-    get fights_go_url
+    f1 = fighters(:one)
+    f2 = fighters(:two)
+    get go_fight_url(f1.id, f2.id)
     assert_response :success
+    ff = Fight.all
+    assert ff.count == 3
   end
 
 end
