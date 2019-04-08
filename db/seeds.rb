b99516738b70959acca895140417bba4a5b9cd0c
Callixte Cauchois <callixte@gmail.com>
2019-04-08 15:43:11 +0200

    Adding some style and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+NAMES = %w[Joe Jean Reginald Archibald Henri David Jérôme Maxime Pierre Louis]
+SURNAMES = %w[Magnifique Brave Fou Rusé Conquerrant Sanguinaire Sauvage Puissant
+  Terrible Barbare Noir Rouge]
+
+Weapon.destroy_all
+Fight.destroy_all
+Fighter.destroy_all
+
+rdm = Random.new
+NAMES.each do |n|
+  name = n + ' le ' + SURNAMES[rdm.rand(8)]
+  life = 70 + rdm.rand(30)
+  force = 70 + rdm.rand(30)
+  dext = 250 - life - force
+  Fighter.create!(
+    name: name,
+    life_points: life,
+    attack_points: force,
+    dexterity: dext
+  )
+end
+
+fighters = Fighter.all
+fighters.each do |f1|
+  f2 = fighters[rdm.rand(fighters.size)]
+  if f1 != f2
+    Fight.start(f1, f2)
+  end
+end
+
+weapons = [
+  [ 'épée', 10, 0 ],
+  [ 'couteau', 3, 0 ],
+  [ 'dague', 5, 0 ],
+  [ 'hache', 13, 0],
+  [ 'bouclier', 0, 10 ]
+]
+weapons.each do |name, attack, defence|
+  Weapon.create!(name: name, attack: attack, defence: defence)
+end
